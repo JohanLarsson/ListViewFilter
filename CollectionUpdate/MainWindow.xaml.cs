8f0b668bee23a903b7c38b3f56f794533a9d64d3
Johan Larsson <johan_gavle1@hotmail.com>
2013-07-11 00:20:48 +0200

    timing

--- CollectionUpdate/MainWindow.xaml.cs
+++ CollectionUpdate/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,6 +45,32 @@ namespace CollectionUpdate
         {
             var itemCollection = List.Items;
             itemCollection.Filter = x => ((Dummy) x).Value%2 == 0;
+        }
+
+        private void Button100_Click(object sender, RoutedEventArgs e)
+        {
+            Add(100);
+        }
+
+        private void Button1000_Click(object sender, RoutedEventArgs e)
+        {
+            Add(1000);
+        }
+
+        private void Button10000_Click(object sender, RoutedEventArgs e)
+        {
+            Add(10000);
+        }
+
+        private void Add(int n)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            _vm.Ints.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                _vm.Ints.Add(new Dummy { Value = i });
+            }
+            Time.Text = string.Format("Adding {0} took {1} ms",n, stopwatch.ElapsedMilliseconds);
         }
     }
 }
