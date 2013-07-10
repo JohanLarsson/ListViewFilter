095f824b13978e8a240ed3f0989769ad398b085e
Johan Larsson <johan_gavle1@hotmail.com>
2013-07-10 20:18:13 +0200

    Initial

---
+++ CollectionUpdate/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CollectionUpdate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Vm _vm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _vm = new Vm();
+            this.DataContext = _vm;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _vm.Ints.Clear();
+            await Task.Delay(100);
+            for (int i = 0; i < 20; i++)
+            {
+                _vm.Ints.Add(i);
+                await Task.Delay(100);
+            }
+        }
+
+        private void Filter_Click(object sender, RoutedEventArgs e)
+        {
+            var itemCollection = List.Items;
+            itemCollection.Filter = x => ((int) x)%2 == 0;
+        }
+    }
+}
---
+++ CollectionUpdate/Vm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CollectionUpdate
+{
+    public class Vm
+    {
+        private ObservableCollection<int> _ints;
+        public ObservableCollection<int> Ints
+        {
+            get { return _ints ?? (_ints = new ObservableCollection<int>()); }
+        }
+    }
+}
